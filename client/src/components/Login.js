d38571bb03ea56792ea4482d3b67552f56e73b91
Priyanka khamkar <118741221+priyankakhamkar16@users.noreply.github.com>
2024-08-29 17:31:08 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../axiosInstance';
 import { AuthContext } from '../AuthContext';
-import './styles/Login.css';  // Import the new CSS
+import './styles/Login.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
