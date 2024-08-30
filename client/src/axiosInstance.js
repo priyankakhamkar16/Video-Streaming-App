2714890a14bf03afe9efb01ec34a73f3d65f8d1a
Priyanka khamkar <118741221+priyankakhamkar16@users.noreply.github.com>
2024-08-30 08:48:23 +0530

    Update axiosInstance.js

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://video-streaming-app-bpp4.vercel.app/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://video-streaming-app-silk.vercel.app/login',
 });
 
 export default axiosInstance;
